e62c15d0f686db9ed088da92960404a59054c148
hourunjing <hourunjing@hourunjingdeMacBook-Pro.local>
2016-12-13 13:48:06 +0800

    选择排序

--- DataStruct&Algorithm/Sort.m
+++ DataStruct&Algorithm/Sort.m
@@ -1,7 +1,5 @@
 #import <Foundation/Foundation.h>
 
-//#define NOR_VALUES @([array[lIndex] integerValue]^[array[rIndex] integerValue])
-
 typedef enum _RandomWay {
     RandomWayIntegerFromZero
 } RandomWay;
@@ -69,8 +67,6 @@ typedef enum _RandomWay {
 
 // 选择排序
 + (void)selectionSort:(NSMutableArray *)originArray;
-// 插入排序
-+ (void)insertionSort:(NSMutableArray *)originArray;
 
 @end
 
@@ -80,7 +76,6 @@ typedef enum _RandomWay {
     for (int i = 0; i < (originArray.count - 1); i++) {
         for (int j = (i + 1); j < originArray.count; j++) {
             if ([originArray[j] compare:originArray[i]] == NSOrderedAscending) {
-//                [Sort swapAtIndex:i and:j inArray:originArray];
                   [originArray exchangeObjectAtIndex:i withObjectAtIndex:j];
             }
         }
@@ -88,45 +83,21 @@ typedef enum _RandomWay {
     NSLog(@"%@",originArray);
 }
 
-+ (void)insertionSort:(NSMutableArray *)originArray {
-    
-    for (int i = 1; i < originArray.count; i++) {
-        for (int j = i; j > 0; j--) {
-            
-            if ([originArray[j] compare:originArray[j-1]] == NSOrderedAscending) {
-//				[Sort swapAtIndex:j and:(j-1) inArray:originArray];
-                [originArray exchangeObjectAtIndex:j withObjectAtIndex:j-1];
-            } else {
-                break;
-            }
-        }
-    }
-    
-    NSLog(@"%@",originArray);
-}
-
-//+ (void)swapAtIndex:(NSInteger)lIndex and:(NSInteger)rIndex inArray:(NSMutableArray *)array {
-//    array[lIndex] = NOR_VALUES;
-//    array[rIndex] = NOR_VALUES;
-//    array[lIndex] = NOR_VALUES;
-//}
-
 @end
 
 int main(int argc, char *argv[]) {
     @autoreleasepool {
 //        NSMutableArray *mutableArray = [@[@8,@6,@2,@3,@1,@5,@7,@4] mutableCopy];
-//        NSMutableArray *mutableArray = [@[@8.2,@6.4,@2,@3,@1,@5,@7,@4] mutableCopy];
-//        NSMutableArray *mutableArray = [@[@"cc",@"fd",@"sgfg",@"swqaa",@"bgf",@"ca"] mutableCopy];
-//        NSMutableArray *mutableArray = [@[[[Student alloc] initWithName:@"sunwei" score:@95],
-//                                          [[Student alloc] initWithName:@"beta" score:@45],
-//                                          [[Student alloc] initWithName:@"gama" score:@86],
-//                                          [[Student alloc] initWithName:@"alpha" score:@73],
-//                                          [[Student alloc] initWithName:@"delta" score:@45],
-//                                          [[Student alloc] initWithName:@"summary" score:@95]] mutableCopy];
+    //        NSMutableArray *mutableArray = [@[@8.2,@6.4,@2,@3,@1,@5,@7,@4] mutableCopy];
+    //        NSMutableArray *mutableArray = [@[@"cc",@"fd",@"sgfg",@"swqaa",@"bgf",@"ca"] mutableCopy];
+    //        NSMutableArray *mutableArray = [@[[[Student alloc] initWithName:@"sunwei" score:@95],
+    //                                          [[Student alloc] initWithName:@"beta" score:@45],
+    //                                          [[Student alloc] initWithName:@"gama" score:@86],
+    //                                          [[Student alloc] initWithName:@"alpha" score:@73],
+    //                                          [[Student alloc] initWithName:@"delta" score:@45],
+    //                                          [[Student alloc] initWithName:@"summary" score:@95]] mutableCopy];
         NSMutableArray *mutableArray = [[RandomData randomArray:RandomWayIntegerFromZero count:10 with:@1000] mutableCopy];
         NSLog(@"%@",mutableArray);
         [Sort selectionSort:mutableArray];
-//        [Sort insertionSort:mutableArray];
     }
 }
